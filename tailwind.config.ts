200aea78c7f4fea2336a83a064add06a93d4c40c
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-09-08 17:07:13 +0330

    feat(layout): Add header image to layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#00ff00', // Green
+        secondary: '#ffffff', // White
+        background: '#413d3a', // Dark Mode
+        'tile-light-grey': '#d3d3d3',
+      },
+      backgroundImage: {
+        'building-wall-tiles': `
+          linear-gradient(to right, #333 1px, transparent 1px), 
+          linear-gradient(to bottom, #333 1px, transparent 1px)
+        `,
+      },
+      backgroundSize: {
+        'tile-size': '100px 50px', // Rectangular tiles (width: 100px, height: 50px)
       },
     },
   },
